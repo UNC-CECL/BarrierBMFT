a44e0be10777e11018b9f49644247fa4f53f8e24
reevesi <reevesi@live.unc.edu>
2021-09-06 18:39:44 -0400

    Initial addition of second marsh to BarrierBMFT

--- barrierbmft/barrierbmft.py
+++ barrierbmft/barrierbmft.py
@@ -23,7 +23,7 @@ def set_yaml(var_name, new_vals, file_name):
         dump(doc, f)
 
 
-def init_equal(bmftc, datadir, input_file):
+def init_equal(bmftc_ML, bmftc_BB, datadir, input_file):
     """Initialize Barrier3D and set identical parameters equal to values in PyBMFT-C"""
 
     fid = datadir + input_file
@@ -32,10 +32,39 @@ def init_equal(bmftc, datadir, input_file):
 
     barrier3d.initialize(fid)
 
-    # Equalize Values of Identical Parameter
-    set_yaml("TMAX", bmftc.dur + 1, fid)  # [yrs] Duration of simulation
-    barrier3d.model._RSLR = np.ones([len(barrier3d.model._RSLR) + 1]) * (bmftc.RSLRi / 1000) / 10 # [m/yr] Relative sea-level rise rate, converted units
-    barrier3d.model._BayDepth = bmftc.Bay_depth[bmftc.startyear - 1] / 10  # [yrs] Initial depth of bay
+    # Check if PyBMFT-C Parameters Are Equal
+    if bmftc_ML.dur != bmftc_BB.dur:
+        print("time_step_count parameters not equal")
+        return
+    if bmftc_ML.RSLRi != bmftc_BB.RSLRi:
+        print("relative_sea_level_rise parameters not equal")
+        return
+    if bmftc_ML.Co != bmftc_BB.Co:
+        print("reference_concentration parameters not equal")
+        return
+    if bmftc_ML.slope != bmftc_BB.slope:
+        print("slope_upland parameters not equal")
+        return
+    if bmftc_ML.bfo != bmftc_BB.bfo:
+        print("bay_fetch_initial parameters not equal")
+        return
+    if bmftc_ML.mwo != bmftc_BB.mwo:
+        print("marsh_width_initial parameters not equal")
+        return
+    if bmftc_ML.wind != bmftc_BB.wind:
+        print("wind_speed parameters not equal")
+        return
+    if bmftc_ML.seagrass_on != bmftc_BB.seagrass_on:
+        print("seagrass_on parameters not equal")
+        return
+    if bmftc_ML.tcr != bmftc_BB.tcr:
+        print("critical_shear_mudflat parameters not equal")
+        return
+
+    # Equalize Barrier3D/PyBMFT-C Values of Identical Parameters
+    set_yaml("TMAX", bmftc_ML.dur + 1, fid)  # [yrs] Duration of simulation
+    barrier3d.model._RSLR = np.ones([len(barrier3d.model._RSLR) + 1]) * (bmftc_ML.RSLRi / 1000) / 10 # [m/yr] Relative sea-level rise rate, converted units
+    barrier3d.model._BayDepth = bmftc_ML.Bay_depth[bmftc_ML.startyear - 1] / 10  # [yrs] Initial depth of bay
 
     return barrier3d
 
@@ -58,16 +87,32 @@ class BarrierBMFT:
         # Initialize Model Classes
 
         # Initialize PyBMFT-C
-        self._bmftc = Bmftc(
-            name="default",
-            time_step=1,
-            time_step_count=50,
-            relative_sea_level_rise=1,
+        # Mainland shoreline
+        self._bmftc_ML = Bmftc(
+            name="mainland",
+            time_step_count=30,
+            relative_sea_level_rise=3,
             reference_concentration=50,
             slope_upland=0.005,
             bay_fetch_initial=3000,
             wind_speed=5,
             seagrass_on=False,
+            forest_on=True,
+            critical_shear_mudflat=0.2,
+            filename_marshspinup="Input/PyBMFT-C/MarshStrat_all_RSLR1_CO50.mat",
+        )
+
+        # Back-barier shoreline
+        self._bmftc_BB = Bmftc(
+            name="back-barrier",
+            time_step_count=30,
+            relative_sea_level_rise=3,
+            reference_concentration=50,
+            slope_upland=0.005,
+            bay_fetch_initial=3000,
+            wind_speed=5,
+            seagrass_on=False,
+            forest_on=True,
             critical_shear_mudflat=0.2,
             filename_marshspinup="Input/PyBMFT-C/MarshStrat_all_RSLR1_CO50.mat",
         )
@@ -77,7 +122,7 @@ class BarrierBMFT:
         input_file = "barrier3d-parameters.yaml"
 
         # Initialize Barrier3D and set matching parameters equal
-        self._barrier3d = init_equal(self._bmftc, datadir, input_file)
+        self._barrier3d = init_equal(self._bmftc_ML, self._bmftc_BB, datadir, input_file)
 
         # ===========================================
         # Initialize variables
@@ -85,7 +130,7 @@ class BarrierBMFT:
         self._bay_unit_slope = bay_unit_slope  # Slope of underlying bay straigraphic unit
         self._bay_unit_depth = bay_unit_thickness  # [m] Depth (stratigraphic height) of underlying bay unit
         self._bay_unit_fine = bay_unit_fine  # Sand proportion, relative to mud, of underlying bay unit
-        self._x_b_TS_bmft = np.zeros([self._bmftc.dur])
+        self._x_b_TS_bmft = np.zeros([self._bmftc_ML.dur])
 
     # ===========================================
     # Time Loop
@@ -97,59 +142,68 @@ class BarrierBMFT:
         self._barrier3d.update()
 
         # Advance PyBMFT-C
-        self._bmftc.update()
+        self._bmftc_ML.update()
 
         if self._coupled:
+
+            """ 
+            6 Sep 21
+            To do:
+                - update x_b (ML) and xm (BB) based on b3d bb shoreline change
+                - incorporate effect of depositional thickness on veg survival (Walters and Kirwan 2016)
+                - update b3d elevation grid based on marsh change
+                - fix overwash deposition within bay
+            """
 
             # ===========================================
             # Adjust fetch and barrier-bay shoreline position in PyBMFT-C according to back-barrier shoreline change
             delta_x_b = (self._barrier3d.model.x_b_TS[-1] - self._barrier3d.model.x_b_TS[-2]) * 10  # [m] Calculate back-barrier shoreline change from Barrier3D; (+) landward movement, (-) seaward movement
-            self._bmftc._bfo = self._bmftc.bfo - int(round(delta_x_b))  # Fetch
-            self._bmftc._x_b = self._bmftc.x_b + int(round(delta_x_b))  # Bay-barrier shoreline
-            self._x_b_TS_bmft[time_step - 1] = self._bmftc.x_b  # Store in array
+            self._bmftc_ML._bfo = self._bmftc_ML.bfo - int(round(delta_x_b))  # Fetch
+            self._bmftc_ML._x_b = self._bmftc_ML.x_b + int(round(delta_x_b))  # Bay-barrier shoreline
+            self._x_b_TS_bmft[time_step - 1] = self._bmftc_ML.x_b  # Store in array
 
             # ===========================================
             # Adjust bay depth in Barrier3D according to depth calculated in PyBMFT-C
-            self._barrier3d.model._BayDepth = self._bmftc.db / 10
+            self._barrier3d.model._BayDepth = np.mean([self._bmftc_ML.db, self._bmftc_BB.db]) / 10
 
             # ===========================================
             # Adjust bay routing width in Barrier3D if fetch in PyBMFTC becomes narrower than 100 m
-            if self._bmftc.bfo < 100:
-                self._barrier3d.model.bay_routing_width = int(round(self._bmftc.bfo / 10))
+            if self._bmftc_ML.bfo < 100:
+                self._barrier3d.model.bay_routing_width = int(round(self._bmftc_ML.bfo / 10))
 
             # ===========================================
             # Adjust proportion of fine sediment ouctropping on Barrier3D shoreface according to bay stratigraphy from PyBMFT-C
 
             # Find thickness of 100% fine sediment deposited since model start
             percent_fine = 1  # IR 24Aug21 To Do: Determine and account for coarse overwash sediment deposition
-            if self._bmftc.x_b < 0:
+            if self._bmftc_ML.x_b < 0:
                 bay_dep_thickness = 0  # Assume eroding only barrier material on shoreface (i.e., 100% sand)
             else:
-                bay_dep_thickness = (self._bmftc.elevation[self._bmftc.startyear + time_step, self._bmftc.x_b] - self._bmftc.elevation[0, self._bmftc.x_b]) * percent_fine  # [m] Sums total deposition since model start
+                bay_dep_thickness = (self._bmftc_ML.elevation[self._bmftc_ML.startyear + time_step, self._bmftc_ML.x_b] - self._bmftc_ML.elevation[0, self._bmftc_ML.x_b]) * percent_fine  # [m] Sums total deposition since model start
 
             # Find thickness of underlying bay unit
-            bay_unit_thickness = self._bay_unit_depth - self._bmftc.x_b * self._bay_unit_slope  # [m]
+            bay_unit_thickness = self._bay_unit_depth - self._bmftc_ML.x_b * self._bay_unit_slope  # [m]
 
             # Calculate total effective thickness of underlying bay unit and newly deposited bay sediment
             fine_thickness = bay_unit_thickness * self._bay_unit_fine + bay_dep_thickness  # [m]
-            if fine_thickness + self._bmftc.db > self._barrier3d.model.DShoreface * 10:  # If the base of the fine unit extends below the shoreface toe
-                fine_thickness = (self._barrier3d.model.DShoreface * 10) - self._bmftc.db  # Thickness of estuarine unit impacting migration can't be greater than the depth of shoreface erosion (i.e., shoreface toe depth)
+            if fine_thickness + self._bmftc_ML.db > self._barrier3d.model.DShoreface * 10:  # If the base of the fine unit extends below the shoreface toe
+                fine_thickness = (self._barrier3d.model.DShoreface * 10) - self._bmftc_ML.db  # Thickness of estuarine unit impacting migration can't be greater than the depth of shoreface erosion (i.e., shoreface toe depth)
 
             # Set fine sediment fraction on the shoreface in Barrier3D (follows Nienhuis & Lorenzo-Trueba, 2019, Geosci. Model Dev.)
-            self._barrier3d.model._shoreface_fine_fraction = fine_thickness / (fine_thickness + self._bmftc.db + self._barrier3d.model.h_b_TS[-1] * 10)
+            self._barrier3d.model._shoreface_fine_fraction = fine_thickness / (fine_thickness + self._bmftc_ML.db + self._barrier3d.model.h_b_TS[-1] * 10)
 
             # ===========================================
             # Add overwash deposition in the bay from Barrier3D to bay of PyBMFT-C -- IR 17 Aug 21: Currently does nothing...
             barrier_transect = np.mean(self._barrier3d.model.InteriorDomain, axis=1) * 10
             overwash_bay_deposition = barrier_transect[np.where(barrier_transect <= 0)[0][0]: np.where(barrier_transect <= 0)[0][-1] + 1]  # [m] Depths of subaqueous cells
-            overwash_bay_deposition -= self._bmftc.elevation[self._bmftc.startyear + time_step - 1, 0: len(overwash_bay_deposition)]  # Extracts overwash bay deposition from subaqueous portion of B3D for this time step
+            overwash_bay_deposition -= self._bmftc_ML.elevation[self._bmftc_ML.startyear + time_step - 1, 0: len(overwash_bay_deposition)]  # Extracts overwash bay deposition from subaqueous portion of B3D for this time step
             overwash_bay_deposition[overwash_bay_deposition < 0] = 0  # Can't have negative deposition
-            self._bmftc.elevation[self._bmftc.startyear + time_step, 0: len(overwash_bay_deposition)] += overwash_bay_deposition  # IR 17 Aug 21: PyBMFTC erases this addition each time-step, so this currently does effectively nothing
+            self._bmftc_ML.elevation[self._bmftc_ML.startyear + time_step, 0: len(overwash_bay_deposition)] += overwash_bay_deposition  # IR 17 Aug 21: PyBMFTC erases this addition each time-step, so this currently does effectively nothing
 
 
     @property
     def bmftc(self):
-        return self._bmftc
+        return self._bmftc_ML
 
     @property
     def barrier3d(self):
