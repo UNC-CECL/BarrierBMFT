d8aa0a3a1ea0e59d8edcff2e4043814f1042c5a6
reevesi <reevesi@live.unc.edu>
2022-01-20 17:10:52 -0500

    Update batch param space plotting script

--- scripts/plot_batch_BarrierBMFT.py
+++ scripts/plot_batch_BarrierBMFT.py
@@ -4,7 +4,7 @@ BarrierBMFT: Coupled Barrier-Bay-Marsh-Forest Model
 Couples Barrier3D (Reeves et al., 2021) with the PyBMFT-C model
 
 Copyright Ian RB Reeves
-Last updated: 6 January 2022
+Last updated: 19 January 2022
 """
 
 import time
@@ -23,95 +23,123 @@ from barrier3d.tools import plot as B3Dfunc
 # Define batch parameters
 
 Num = 1  # Number of runs at each combinations of parameter values
-SimDur = 125  # [Yr] Duration of each simulation
+SimDur = 100  # [Yr] Duration of each simulation
 
 # Parameter values
-# rslr = [2, 6, 10, 14, 18]
-# co = [10, 30, 50, 70, 90]
-# slope = [0.001, 0.003, 0.005, 0.007, 0.009]
-rslr = [2, 10, 18]
-co = [10, 50, 90]
-slope = [0.003]
+rslr = [1, 3, 5, 7, 9]
+co = [15, 30, 45, 60, 75]
+slope = [0.005]
 
 SimNum = len(rslr) * len(co) * len(slope)
 
 # ==================================================================================================================================================================================
 # Load data
-filename = '/Users/reevesi/PycharmProjects/BarrierBMFT/Output/Batch_2022_0107_18_39/'
+filename = '/Users/ianreeves/PycharmProjects/BarrierBMFT/Output/Batch_2022_0118_17_15/'
 
 BarrierWidth = np.load(filename + 'Widths_Barrier.npy')
-BarrierWidth = BarrierWidth[0, :, :, 0]
+BarrierWidth = BarrierWidth[0, :, :, 0] * -1
+BarrierWidth = np.rot90(BarrierWidth)
+BarrierWidth = np.flipud(BarrierWidth)
 
 BBMarshWidth = np.load(filename + 'Widths_BBMarsh.npy')
-BBMarshWidth = BBMarshWidth[0, :, :, 0]
+BBMarshWidth = BBMarshWidth[0, :, :, 0] * -1
+BBMarshWidth = np.rot90(BBMarshWidth)
+BBMarshWidth = np.flipud(BBMarshWidth)
 
 BayWidth = np.load(filename + 'Widths_Bay.npy')
-BayWidth = BayWidth[0, :, :, 0]
+BayWidth = BayWidth[0, :, :, 0] * -1
+BayWidthOrig = BayWidth
+BayWidth = np.rot90(BayWidth)
+BayWidth = np.flipud(BayWidth)
 
 MLMarshWidth = np.load(filename + 'Widths_MLMarsh.npy')
-MLMarshWidth = MLMarshWidth[0, :, :, 0]
+MLMarshWidth = MLMarshWidth[0, :, :, 0] * -1
+MLMarshWidth = np.rot90(MLMarshWidth)
+MLMarshWidth = np.flipud(MLMarshWidth)
 
 ForestWidth = np.load(filename + 'Widths_Forest.npy')
-ForestWidth = ForestWidth[0, :, :, 0]
+ForestWidth = ForestWidth[0, :, :, 0] * -1
+ForestWidthOrig = ForestWidth
+ForestWidth = np.rot90(ForestWidth)
+ForestWidth = np.flipud(ForestWidth)
 
 ShorelineChange = np.load(filename + 'ShorelineChange.npy')
 ShorelineChange = ShorelineChange[0, :, :, 0]
+ShorelineChange = np.rot90(ShorelineChange)
+ShorelineChange = np.flipud(ShorelineChange)
 
+
+# SimEl = np.load(filename + 'Sim25_elevation.npy', allow_pickle=True)
 
 # ==================================================================================================================================================================================
 # Plot
 
-xtic = ['', '2', '10', '18']
-ytic = ['', '10', '50', '90']
+xtic = ['', '1', '3', '5', '7', '9']
+ytic = ['', '15', '30', '45', '60', '75']
 
 xlab = 'RSLR [mm/yr]'
-ylab = 'Susp. Sed. Concentration [mg/L]'
+ylab = 'Back-Barrier SSC [mg/L]'
 
 all = np.concatenate((BarrierWidth, BBMarshWidth, BayWidth, MLMarshWidth, ForestWidth))
 vmin = int(np.min(all))
 vmax = int(np.max(all))
+maximum = max(abs(vmin), abs(vmax))
+vmax = maximum
+vmin = -maximum
 
-Fig = plt.figure(figsize=(13.5, 3))
-plt.rcParams.update({'font.size': 10})
+
+cmap = 'RdBu'
+Fig = plt.figure(figsize=(28, 6))
+plt.rcParams.update({'font.size': 14, 'font.family': 'Arial'})
 
 ax = Fig.add_subplot(151)
-ax.matshow(BarrierWidth, origin='lower', cmap='Greys', aspect='auto')  # , vmin=vmin, vmax=vmax)
+cax = ax.matshow(BarrierWidth, origin='lower', cmap=cmap, aspect='auto', vmin=vmin, vmax=vmax)
 ax.xaxis.set_ticks_position('bottom')
+# cbar = Fig.colorbar(cax)
+# cbar.set_label('Change in Width', rotation=270, labelpad=20)
 ax.set_xticklabels(xtic)
 ax.set_yticklabels(ytic)
 plt.ylabel(ylab)
 plt.title('Barrier')
 
+
 ax = Fig.add_subplot(152)
-ax.matshow(BBMarshWidth, origin='lower', cmap='Greys', aspect='auto')  # , vmin=vmin, vmax=vmax)
+cax = ax.matshow(BBMarshWidth, origin='lower', cmap=cmap, aspect='auto', vmin=vmin, vmax=vmax)
 ax.xaxis.set_ticks_position('bottom')
+# cbar = Fig.colorbar(cax)
+# cbar.set_label('Change in Width', rotation=270, labelpad=20)
 ax.set_xticklabels(xtic)
 ax.set_yticklabels(ytic)
 plt.title('Back-Barrier Marsh')
 
 ax = Fig.add_subplot(153)
-ax.matshow(BayWidth, origin='lower', cmap='Greys', aspect='auto')  # , vmin=vmin, vmax=vmax)
+cax = ax.matshow(BayWidth, origin='lower', cmap=cmap, aspect='auto', vmin=vmin, vmax=vmax)
 ax.xaxis.set_ticks_position('bottom')
+# cbar = Fig.colorbar(cax)
+# cbar.set_label('Change in Width', rotation=270, labelpad=20)
 ax.set_xticklabels(xtic)
 ax.set_yticklabels(ytic)
 plt.xlabel(xlab)
 plt.title('Bay')
 
 ax = Fig.add_subplot(154)
-ax.matshow(MLMarshWidth, origin='lower', cmap='Greys', aspect='auto')  # , vmin=vmin, vmax=vmax)
+cax = ax.matshow(MLMarshWidth, origin='lower', cmap=cmap, aspect='auto', vmin=vmin, vmax=vmax)
 ax.xaxis.set_ticks_position('bottom')
+# cbar = Fig.colorbar(cax)
+# cbar.set_label('Change in Width', rotation=270, labelpad=20)
 ax.set_xticklabels(xtic)
 ax.set_yticklabels(ytic)
 plt.title('Mainland Marsh')
 
 ax = Fig.add_subplot(155)
-cax = ax.matshow(ForestWidth, origin='lower', cmap='Greys', aspect='auto')  # , vmin=vmin, vmax=vmax)
+cax = ax.matshow(ForestWidth, origin='lower', cmap=cmap, aspect='auto', vmin=vmin, vmax=vmax)
 ax.xaxis.set_ticks_position('bottom')
 cbar = Fig.colorbar(cax)
-cbar.set_label('Change in Width', rotation=270, labelpad=20)
+cbar.set_label('Change in Width [m]', rotation=270, labelpad=20)
 ax.set_xticklabels(xtic)
 ax.set_yticklabels(ytic)
 plt.title('Forest')
+plt.tight_layout()
 
 # sumplot = BarrierWidth + BBMarshWidth + BayWidth + MLMarshWidth + ForestWidth
 # ax = Fig.add_subplot(166)
@@ -124,7 +152,15 @@ plt.title('Forest')
 # plt.xlabel(xlab)
 # plt.title('')
 
-plt.tight_layout()
-print()
+
+# plt.figure(figsize=(12, 6))
+# plt.xlabel("Distance Cross-Shore [m]")
+# plt.ylabel("Elevation [m]")
+# plt.title("Sim 25")
+# for t in range(0, SimDur, 10):
+#     elev = SimEl[t]
+#     plt.plot(elev)
+
+
 plt.show()
 
